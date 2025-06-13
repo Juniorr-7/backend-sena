b90c380594fc1bb8da36a9e9c8cddc9e65fc28aa
Juniorr-7 <juniorfajardom@gmail.com>
2025-06-12 21:13:28 -0500

    Index creado con importación dinámica

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// Importación dinámica de rutas
+const routersPath = path.join(__dirname, 'routers');
+
+fs.readdirSync(routersPath).forEach(async (file) => {
+  if (file.endsWith('.js')) {
+    const { default: router } = await import(path.join(routersPath, file));
+    app.use(router);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port", process.env.PORT);
+});
