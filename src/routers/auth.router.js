5081d8e8614b1c0b28ec91c29d6b4b593aba1d87
Juniorr-7 <juniorfajardom@gmail.com>
2025-06-14 23:35:29 -0500

    Ahora sólo se puede cerrar sesión si existe un token en el header

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { login, logout } from "../controllers/auth.controller.js";
+import { verifyToken } from "../middlewares/jsonwebtoken.middleware.js";
 
 const router = Router();
 
 router.post("/login", login);
-router.post("/logout", logout);
+router.post("/logout", verifyToken, logout);
 
 export default { prefix: "/auth", router };
